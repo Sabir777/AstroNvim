a179bdf176c4c11942127f328efc86ce94fb3144
Sabir777 <sabir.yanin2014@yandex.ru>
2026-02-19 13:31:13 +0300

    Старт: Есть Python, русский язык, глобальный буфер, перемещение в начало и конец строки

---
+++ lua/polish.lua
@@ -0,0 +1,8 @@
+
+-- This will run last in the setup process.
+-- This is just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+
+-- Включаю русский язык
+vim.opt.langmap = "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ;`qwertyuiop[]asdfghjkl\\;'zxcvbnm\\,.~QWERTYUIOP{}ASDFGHJKL:\\\"ZXCVBNM<>"
+
