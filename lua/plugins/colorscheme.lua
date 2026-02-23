cccb526d136c2e2ccae4e2fa3c331e3c75aa6780
Sabir777 <sabir.yanin2014@yandex.ru>
2026-02-24 01:15:37 +0300

    Выполнил правильно добавление цветовой схемы (пропали ошибки)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
 return {
-  { "folke/tokyonight.nvim", opts = { style = "night" } },
-  { "LazyVim/LazyVim", opts = { colorscheme = "tokyonight" } },
+  { "folke/tokyonight.nvim",
+  opts = { style = "night" } },
 }
