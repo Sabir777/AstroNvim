d083b518daf7b003e48d5b871c07d9d0bb1bfafb
Sabir777 <sabir.yanin2014@yandex.ru>
2026-02-26 01:01:33 +0300

    Добавил обычную нумерацию строк и русификацию

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Возвращаю обычную нумерацию строк
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+-- Включаю русский язык
+vim.opt.langmap = "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ;`qwertyuiop[]asdfghjkl\\;'zxcvbnm\\,.~QWERTYUIOP{}ASDFGHJKL:\\\"ZXCVBNM<>"
