144027b94f58f95ad1cb8acb485089c7680df9b4
Sabir777 <sabir.yanin2014@yandex.ru>
2026-02-26 00:22:57 +0300

    Добавил копирование-вставку для глобального буфера обмена

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Копировать в глобальный буфер обмена
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to clipboard" })
+
+-- Вставить из глобального буфера обмена
+vim.keymap.set({"n", "v"}, ",v", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("c", ",v", "<C-r>+", { desc = "Paste from clipboard in cmdline" })
