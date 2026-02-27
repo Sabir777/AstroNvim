4e2279e5320b81612f6f0f382881ce6f4ef0df62
Sabir777 <sabir.yanin2014@yandex.ru>
2026-02-28 01:01:32 +0300

    Разрешил перенос строк для отображения длинных строк без разбивки на новые строки

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,8 +9,12 @@ vim.opt.relativenumber = false
 
 -- Делаю визуальный отступ для курсора от границ экрана
 vim.opt.scrolloff = 5
+
 -- Задаю ширину строки
-vim.opt.textwidth = 120
+-- vim.opt.textwidth = 120
+
+-- Разрешаю перенос текста без разбивки строки
+vim.opt.wrap = true
 
 -- Включаю русский язык
 vim.opt.langmap = "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ;`qwertyuiop[]asdfghjkl\\;'zxcvbnm\\,.~QWERTYUIOP{}ASDFGHJKL:\\\"ZXCVBNM<>"
